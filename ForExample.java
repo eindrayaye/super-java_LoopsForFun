dc77d2783de7ee5670c337669a5d153394a9b666
EindrayAye <eindray.aye@g.austincc.edu>
2020-08-12 16:30:05 -0500

    Create ForExample.java

---
+++ ForExample.java
@@ -0,0 +1,40 @@
+public class ForExample {
+	public static void main(String[] args) {
+
+		ForExample cast = new ForExample();
+		cast.num1();
+	}
+
+public void num1()
+		{
+		for(int i=0; i< 6; i++)
+			switch (i){
+			case 0:
+				System.out.println("This is i and the value is " + i);
+			break;
+			
+			case 1 :
+			    System.out.println("This is i and the value is " + i);
+			    break;
+			    
+			case 2 :
+			    System.out.println("This is i and the value is " + i);
+			    break;
+			    
+			case 3 :
+			    System.out.println("This is i and the value is " + i);
+			    break;
+			    
+			case 4 :
+			    System.out.println("This is i and the value is " + i);
+			    break;
+			    
+			case 5 :
+			    System.out.println("This is i and the value is " + i);
+			    break;
+			    
+			default :
+			    System.out.println("Done");
+		       }
+		}
+}
