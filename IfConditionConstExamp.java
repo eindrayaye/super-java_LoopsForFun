d0f375a6919c9241f667e5575c20c77411b30c1c
EindrayAye <eindray.aye@g.austincc.edu>
2020-08-12 17:16:33 -0500

    Update IfConditionConstExamp.java

--- IfConditionConstExamp.java
+++ IfConditionConstExamp.java
@@ -1,30 +1,41 @@
+import java.util.Scanner;
+
 public class IfConditionConstExamp {
 
 	public static void main(String[] args) {
 		int a=5;
 		int b=10;
-		if (a >10)
-			System.out.println(a + " is greater than " + b);
-		else 
-			System.out.println(b + " is greater than " + a);
+		int x;
+		int n;
+		
+		System.out.println ("Let's Play number game!!!!");
+		Scanner sc=new Scanner(System.in);
+		System.out.println(" TYPE NUMBER HERE 1 OR 2 ?");
+		n=sc.nextInt();
+		
+		if (n == 1) {
+			System.out.println(" YEA YOU WIN \\(^__^)// " );
+		} else {
+		 	System.out.println(" SERIOUSLY ? <<(0 __ 0)>> " );
+		   }
 			
-	//} 
+		System.out.println("************");
+		System.out.println ("Let's Play another game? Why not?");
+		sc=new Scanner(System.in);
+		System.out.println("What month is it now? Type in NUMBER ===>> ");
+		System.out.println(" ");
+		x=sc.nextInt();
 		
-		//int a=12;
-		//if(a>=10)
-		//{
-		//if(a<=20)
-		//System.out.println("value of a is in range: 10 to 20");
-		//} 
-		
-		int month = 4;
+		int month=x;;
 		String season;
 		if(month==12||month==1||month==2)
 			season = "Winter";
 		else if(month==3||month == 4|| month == 5)
 			season = "Spring";
+		else if(month==6||month == 7|| month == 8)
+			season = "Summer : Dmmm!! Hot like Hell in Texas";
 		else
-			season = "Ahhhh WHAT?";
-		System.out.println("April is the : " + season);
+			season = "Ahhhh WHAT now?";
+		System.out.println("It's : " + season);
 	}
 }
